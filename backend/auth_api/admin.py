322d61ade32f7edf4ed5a1cd63d3801579f6cb5f
Ybuica <ktymof@gmail.com>
2024-12-02 14:51:23 +0300

    ref: change admin

--- backend/auth_api/admin.py
+++ backend/auth_api/admin.py
@@ -1,27 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from rest_framework.authtoken.models import TokenProxy
 
 from .models import User
+from .admin_custom import CustomUserAdmin
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ('username', 'email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active'),
-        }),
-    )
-    search_fields = ('username', 'email')
-    ordering = ('username',)
-    filter_horizontal = ('groups', 'user_permissions')
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.unregister(TokenProxy)
